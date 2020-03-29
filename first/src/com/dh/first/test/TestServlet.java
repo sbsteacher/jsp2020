4133683a513ce39c2e8f018edf49f5e751b7c23e
찬웅 안 <cksdnd3052@gmail.com>
2020-03-29 13:21:33 +0900

    디비 작업(1) 완성

--- first/src/com/dh/first/test/TestServlet.java
+++ first/src/com/dh/first/test/TestServlet.java
@@ -1,6 +1,8 @@
 package com.dh.first.test;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,12 +14,16 @@ public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/test/write.jsp");		
+		rd.forward(request, response);
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		String pk = request.getParameter("pk");
+		String val = request.getParameter("val");
+		System.out.println("pk : " + pk);
+		System.out.println("val : " + val);
 	}
 
 }
