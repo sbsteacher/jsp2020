c401c894cb248ffe8fea04f5cc292c335d2f6a59
찬웅 안 <cksdnd3052@gmail.com>
2020-04-04 12:39:17 +0900

    모델 사용(1)

--- first/src/com/dh/first/db/TestDAO.java
+++ first/src/com/dh/first/db/TestDAO.java
@@ -3,25 +3,40 @@ package com.dh.first.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
+import com.dh.first.test.TestVO;
+
 public class TestDAO {
-	public static void insert(int pk, String val) {
+	public static void insert(TestVO vo) {
 		Connection con = null;
 		PreparedStatement ps = null;
+		
+		if(val.indexOf(";") > 0) {
+			return;
+		}
+		/*
 		String sql = " INSERT INTO test "
 				+ " (pk, val)"
 				+ " VALUES"
 				+ " ( " + pk + ", '" + val +"') ";		
-		/*
-		sql = String.format(" INSERT INTO test "
+		
+		
+		String sql = String.format(" INSERT INTO test "
 				+ " (pk, val)"
 				+ " VALUES"
 				+ " ( %d, '%s') ", pk, val);
 		*/
+		String sql = " INSERT INTO test "
+				+ " (pk, val)"
+				+ " VALUES"
+				+ " (?, ?) ";		
 		
 		System.out.println("sql : " + sql);
 		try {
 			con = Conn.getCon();			
 			ps = con.prepareStatement(sql);
+			ps.setInt(1, pk);
+			ps.setString(2, val);
+			
 			ps.execute();			
 		} catch (Exception e) {			
 			e.printStackTrace();
---
+++ first/src/com/dh/first/test/TestVO.java
@@ -0,0 +1,27 @@
+package com.dh.first.test;
+
+public class TestVO {
+	private int pk;
+	private String val;
+	private String r_dt;
+	
+	public int getPk() {
+		return pk;
+	}
+	public void setPk(int pk) {
+		this.pk = pk;
+	}
+	public String getVal() {
+		return val;
+	}
+	public void setVal(String val) {
+		this.val = val;
+	}
+	public String getR_dt() {
+		return r_dt;
+	}
+	public void setR_dt(String r_dt) {
+		this.r_dt = r_dt;
+	}	
+	
+}
